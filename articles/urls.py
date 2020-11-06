d9b7b3c7f564f60582b181400be184e3db03ed10
hareesh43 <hareeshgummani@gmail.com>
2020-11-06 17:23:54 +0530

    added static files

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from .import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('',views.article_list)
+    ]
 
-]
+urlpatterns += staticfiles_urlpatterns()
