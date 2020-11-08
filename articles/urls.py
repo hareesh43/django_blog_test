21c125694136512b6786eed96e7bf504da9c97fd
hareesh43 <hareeshgummani@gmail.com>
2020-11-08 17:06:36 +0530

    article details url

--- articles/urls.py
+++ articles/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+app_name = 'articles'
 
 urlpatterns = [
-    path('',views.article_list)
+    path('',views.article_list,name ='article_list'),
+    path('<str:slug_thing>/',views.article_details,name='details')
     ]
 
 urlpatterns += staticfiles_urlpatterns()
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 def article_list(request):
 
     articles = Article.objects.all().order_by('date')
     return render(request,'articles/articles_list.html',{'articles':articles})
+
+def article_details(request,slug_thing):
+    # return HttpResponse(slug_thing)
+    article = Article.objects.get(slug=slug_thing)
+    return render(request,'articles/article_details.html',{'article':article})
