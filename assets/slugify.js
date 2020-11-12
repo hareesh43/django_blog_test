d8c2598fd2bd83d27c854f07a287880237fc8100
hareesh43 <hareeshgummani@gmail.com>
2020-11-12 17:58:56 +0530

    added automatic slug

---
+++ assets/slugify.js
@@ -0,0 +1,16 @@
+const titleinput = document.querySelector('input[name="title"]');
+const sluginput = document.querySelector('input[name="slug"]');  
+
+const slugify = (val)=>
+{
+    return val.toString().toLowerCase().trim().replace(/&/g,'-and-') //replace & with -and-
+    .replace(/[\s\W-]+/g,'-')
+    .replace() //replace non word character space to -
+
+}
+
+titleinput.addEventListener("keyup",(e)=>
+{
+    sluginput.setAttribute('value',slugify(titleinput.value));
+
+});
